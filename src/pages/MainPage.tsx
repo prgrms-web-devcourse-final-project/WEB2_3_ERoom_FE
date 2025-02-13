17f723e6e260db3452494db63322d765bedc1875
kkyuheak <gyuheak@gmail.com>
2025-02-13 17:20:00 +0900

    design: 메인페이지 로그인상태, 레이아웃 디자인 #5

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,32 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import TodaySchedule from "../components/MainPage/TodaySchedule";
+
+const MainPage = () => {
+  return (
+    <div
+      className="px-5 bg-gray-100 flex gap-2"
+      style={{ height: "calc(100vh - 50px)" }}
+    >
+      {/* 캘린더 */}
+      <div className="flex-1 px-[60px] flex flex-col gap-2">
+        <div className="flex justify-end">
+          <ul className="flex gap-2 font-bold">
+            <li className="bg-[#a1a1a1] px-3 py-1 cursor-pointer">프로젝트</li>
+            <li className="bg-[#a1a1a1] px-3 py-1 cursor-pointer">개인업무</li>
+          </ul>
+        </div>
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          height={"calc(100vh - 50px)"}
+        />
+      </div>
+
+      {/* 오늘의 일정 */}
+      <TodaySchedule />
+    </div>
+  );
+};
+
+export default MainPage;
