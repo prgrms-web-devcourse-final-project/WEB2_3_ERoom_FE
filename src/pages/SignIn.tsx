f08b87f1ca4ba38f8d9bb0ee1b035bb4fb1c868f
sunhyeong <parksunhyeong529@gmail.com>
2025-02-14 08:36:54 +0900

    design: auth퍼블리싱중 #12

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,16 +1,36 @@
 import Button from "../components/common/Button";
+import "../styles/AuthLayout.css";
+import KakaoLogo from "../assets/kakao_logo.svg";
+import GoogleLogo from "../assets/google_logo.svg";
 
 const SignIn = () => {
   return (
-    <div className=" w-[520px] h-[440px] bg-[#ffffff94] p-[100px]">
+    <div className=" w-[554px] h-[414px] bg-[#ffffff94] px-[100px] py-[50px] rounded-[10px]">
       <div className="">
-        <div className="flex flex-col gap-[20px] pb-[40px] border-b-[1px]">
-          <Button text="카카오 계정으로 로그인" size="lg" />
-          <Button text="Google 계정으로 로그인" size="lg" />
+        <div className="text-center mb-[50px]">
+          <span className="text-logo-green-light font-bold text-[20px]">
+            LOGIN
+          </span>
         </div>
-        <div className="flex flex-col pt-[40px] gap-[5px]">
-          <span className="text-sm text-center">아직 회원이 아니신가요?</span>
-          <Button text="회원가입하러 가기" to="/signUp" size="lg" />
+        <div className="relative flex flex-col gap-[20px] pb-[40px] border-b-[1px]">
+          <Button
+            text="카카오 로그인"
+            size="lg"
+            css="bg-kakao border-gray01 text-black gap-[10px]"
+            logo={KakaoLogo}
+          />
+          <Button
+            text="구글 로그인"
+            size="lg"
+            css="bg-white border-gray01 text-black gap-[10px]"
+            logo={GoogleLogo}
+          />
+        </div>
+        <div className="relative flex flex-col pt-[40px] gap-[5px]">
+          <span className="text-sm text-center font-semibold">
+            아직 회원이 아니신가요?
+          </span>
+          <Button text="회원가입" to="/signUp" size="lg" />
         </div>
       </div>
     </div>
