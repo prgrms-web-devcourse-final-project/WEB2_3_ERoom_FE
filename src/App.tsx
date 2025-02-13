57c547b0af9dd20e5cdf2b34086fe80d3b006df5
sunhyeong <parksunhyeong529@gmail.com>
2025-02-13 17:22:18 +0900

    design: 회원가입레이아웃 #8

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,18 @@ import { Route, Routes } from "react-router";
 import Main from "./pages/Main";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import AuthLayout from "./layouts/AuthLayout";
+import SignUpCompanyInfo from "./pages/SignUpCompanyInfo";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="signIn" element={<SignIn />} />
-      <Route path="signUp" element={<SignUp />} />
+      <Route element={<AuthLayout />}>
+        <Route path="signIn" element={<SignIn />} />
+        <Route path="signUp" element={<SignUp />} />
+        <Route path="signUpCompanyInfo" element={<SignUpCompanyInfo />} />
+      </Route>
     </Routes>
   );
 };
