6508f45ec24f85ff6326e865410ffd2326314778
sunhyeong <parksunhyeong529@gmail.com>
2025-02-13 21:27:13 +0900

    design: AuthLayout배경투명도오류공#12

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -1,12 +1,22 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
+import "../../styles/AuthLayout.css";
+import BackgroundImg from "../../assets/auth_background.svg";
 
 const AuthLayout = () => {
   return (
     <>
       <Header />
-      <section className="flex justify-center items-center h-screen">
-        <Outlet />
+      <section
+        className="relative flex justify-center items-center h-screen bg-no-repeat bg-cover bg-center"
+        style={{
+          backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.2), rgba(255, 255, 255, 0.2)), url(${BackgroundImg})`,
+          backgroundBlendMode: "overlay",
+        }}
+      >
+        <div className="relative z-10">
+          <Outlet />
+        </div>
       </section>
     </>
   );
