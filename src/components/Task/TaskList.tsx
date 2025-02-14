00417d4aa663355605eecf0af093aea82139480a
kkyuheak <gyuheak@gmail.com>
2025-02-14 23:57:53 +0900

    design: 프로젝트 상세페이지 담당자 디자인 #14

---
+++ src/components/Task/TaskList.tsx
@@ -0,0 +1,21 @@
+import TaskBox from "./TaskBox";
+
+interface TaskListProps {
+  name: string;
+  isAll?: boolean;
+}
+
+const TaskList = ({ name, isAll = true }: TaskListProps) => {
+  return (
+    <div className="flex flex-col gap-4 items-center px-2 py-1">
+      <h1 className="font-bold text-main-green text-[22px]">{name}</h1>
+      <TaskBox isAll={isAll} />
+      <TaskBox isAll={isAll} />
+      <TaskBox isAll={isAll} />
+      <TaskBox isAll={isAll} />
+      <TaskBox isAll={isAll} />
+    </div>
+  );
+};
+
+export default TaskList;
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,35 +1,54 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router";
+import { Link, useLocation } from "react-router";
+import outProjectIcon from "../../assets/icons/outProjectDetail.svg";
+import sidebarAllicon from "../../assets/icons/sidebarAllicon.svg";
+import sidebarManagerIcon from "../../assets/icons/sidebarManagerIcon.svg";
+import sidebarMeetIcon from "../../assets/icons/sidebarMeetIcon.svg";
+import { twMerge } from "tailwind-merge";
 
 const SideMenuList = [
-  { title: "전체 업무", icon: "" },
-  { title: "담당자", icon: "" },
-  { title: "미팅룸", icon: "" },
+  { title: "전체 업무", icon: sidebarAllicon, src: "all" },
+  { title: "담당자", icon: sidebarManagerIcon, src: "manager" },
+  { title: "미팅룸", icon: sidebarMeetIcon, src: "meeting" },
 ];
 
 const Sidebar = () => {
   const { pathname } = useLocation();
 
-  const [path, setPath] = useState(pathname);
+  // const [path, setPath] = useState(pathname);
 
+  // useEffect(() => {
+  //   setPath(pathname);
+  // }, [pathname]);
   useEffect(() => {
-    setPath(pathname);
-  }, [pathname]);
-
+    console.log(pathname);
+  });
   return (
     <div
-      className="w-[130px] bg-white"
+      className="w-[130px] bg-white border"
       style={{ height: "calc(100vh - 50px)" }}
     >
-      {path.startsWith("/projectRoom") ? (
+      {pathname.startsWith("/projectRoom") ? (
         <ul className="flex flex-col items-center gap-6 pt-10">
+          <li>
+            <Link to="/projectRoom" className="flex flex-col items-center">
+              <img src={outProjectIcon} alt="프로젝트 나가기 버튼" />
+              <p className="font-bold text-main-green01">My프로젝트</p>
+            </Link>
+          </li>
           {SideMenuList.map((menu, idx) => {
             return (
               <li
                 key={idx}
                 className="font-bold w-full h-[30px] flex px-4 items-center cursor-pointer"
               >
-                {menu.title}
+                <Link
+                  to={`${pathname}?category=${menu.src}`}
+                  className={twMerge(`flex items-center gap-2`)}
+                >
+                  <img src={menu.icon} alt="사이드바 메뉴 아이콘" />
+                  <p className="text-header-green">{menu.title}</p>
+                </Link>
               </li>
             );
           })}
