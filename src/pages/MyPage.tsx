8822185808ac40a90b455a5cb62bc855002c50cc
sunhyeong <parksunhyeong529@gmail.com>
2025-02-14 12:20:53 +0900

    design: 마이페이지퍼블리싱 #16

---
+++ src/pages/MyPage.tsx
@@ -0,0 +1,70 @@
+import { useState } from "react";
+import Button from "../components/common/Button";
+import "../styles/AuthLayout.css";
+import DefaultImg from "../assets/sample_default_profile.png";
+
+const MyPage = () => {
+  const [companyInfo, setCompanyInfo] = useState<string | undefined>("");
+  const [progileImg, setProfileImg] = useState("");
+
+  const handleCompanyInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCompanyInfo(e.target.value);
+  };
+  //프로필 이미지 수정 함수(추후 구현)
+  const handleProfileImg = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setProfileImg(e.target.value);
+  };
+
+  return (
+    <section className="mypage-background flex justify-center items-center relative min-h-screen">
+      <div className="absolute inset-0 blur"></div>
+      <div className="relative z-10">
+        <div className="flex flex-col justify-center items-center w-[680px] h-[559px] bg-[#ffffff94] p-[100px] rounded-[10px]">
+          <div className="text-center">
+            <span className="text-logo-green-light font-bold text-[20px]">
+              개인정보
+            </span>
+          </div>
+          <div className="flex gap-[30px] my-[50px]">
+            <div className="w-[200px] h-[206px] overflow-hidden cursor-pointer">
+              {/* 프로필 기본 이미지 샘플로 넣어둠. 추후 기본이미지 나오면 수정 필요 */}
+              <img
+                src={progileImg || DefaultImg}
+                alt="프로필 이미지"
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="flex flex-col gap-[20px]">
+              <div className="flex flex-col gap-[10px]">
+                <span className="font-bold">이름</span>
+                <div className="pl-[10px]">
+                  <span className="text-black01">홍길동</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-[10px]">
+                <span className="font-bold">이메일</span>
+                <div className="pl-[10px]">
+                  <span className="text-black01">hong@gmail.com</span>
+                </div>
+              </div>
+              <div className="flex flex-col gap-[10px]">
+                <span className="font-bold">소속</span>
+                <input
+                  type="text"
+                  value={companyInfo}
+                  onChange={handleCompanyInfo}
+                  className="w-[250px] h-[33px] pl-[10px] bg-transparent focus:outline-none border-b-[1px] border-b-gray01"
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <Button text="등록하기" size="md" to="/" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default MyPage;
