8a2bbd8b3cacb42b5340e2970e9990dd78a91884
kkyuheak <gyuheak@gmail.com>
2025-02-15 22:47:49 +0900

    design: 프로젝트룸 페이지 디자인 수정 #21

--- src/components/ProjectRoom/ProjectListBox.tsx
+++ src/components/ProjectRoom/ProjectListBox.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import ProjectProgressBar from "./ProjectProgressBar";
 import ParticipantIcon from "../common/ParticipantIcon";
+import Button from "../common/Button";
 
 interface ProjectListBoxProps {
   projectId: number;
@@ -10,44 +11,54 @@ const ProjectListBox = ({ projectId }: ProjectListBoxProps) => {
   return (
     <div
       className="bg-white border border-[#CAD2CB] w-full h-[70px] flex 
-        justify-between items-center px-10 font-bold flex-shrink-0"
+        gap-5 items-center px-5 font-bold "
     >
       {/* 프로젝트 넘버 */}
+      <p className="text-[50px] font-medium text-main-green02">10</p>
 
-      {/* 프로젝트 명, 기간 */}
-      <div>
-        <p>프로젝트 명</p>
-        <p>2025.02.03 ~ 2025.03.12</p>
-      </div>
-
-      {/* 진행률 */}
-      <div className="flex flex-col items-center gap-1">
-        <div className="flex justify-center gap-2">
-          <p>진행률</p>
-          <p>50.0%</p>
+      <div className="flex justify-between items-center w-full px-5">
+        {/* 프로젝트 명, 기간 */}
+        <div>
+          <p>프로젝트 명</p>
+          <p>2025.02.03 ~ 2025.03.12</p>
         </div>
-        <ProjectProgressBar />
-      </div>
 
-      {/* 참여인원 */}
-      <div className=" flex items-center gap-4">
-        <p>참여인원</p>
-        <div className="flex">
-          <ParticipantIcon css="" />
-          <ParticipantIcon css="ml-[-5px] bg-red-100" />
-          <ParticipantIcon css="ml-[-5px] bg-blue-100" />
-          <ParticipantIcon css="ml-[-5px] bg-green-100" />
-          <ParticipantIcon css="ml-[-5px] bg-pink-100" />
+        {/* 진행률 */}
+        <div className="flex flex-col items-center gap-1">
+          <div className="flex justify-center gap-2">
+            <p>진행률</p>
+            <p>50.0%</p>
+          </div>
+          <ProjectProgressBar />
+        </div>
+
+        {/* 참여인원 */}
+        <div className=" flex items-center gap-4">
+          <p>참여인원</p>
+          <div className="flex">
+            <ParticipantIcon css="" />
+            <ParticipantIcon css="ml-[-5px] bg-red-100" />
+            <ParticipantIcon css="ml-[-5px] bg-blue-100" />
+            <ParticipantIcon css="ml-[-5px] bg-green-100" />
+            <ParticipantIcon css="ml-[-5px] bg-pink-100" />
+          </div>
+        </div>
+
+        <div className="flex gap-3 items-center">
+          <Button
+            text="수정"
+            size="md"
+            css="h-[40px] w-[48px] border-main-green02 text-main-green01"
+          />
+          <Link
+            to={`/meetingRoom/${projectId}`}
+            className="h-[40px] bg-[#FFFCE2] text-main-green01 flex items-center justify-center
+          border border-main-green01 px-[10px] font-bold rounded-sm"
+          >
+            미팅룸 입장
+          </Link>
         </div>
       </div>
-
-      {/* 프로젝트 입장 임시 url설정 */}
-      <Link
-        to={`/projectRoom/${projectId}`}
-        className="bg-[#cad2cb70] text-[#6E8370] border border-[#CAD2CB] p-[10px] font-bold rounded-sm"
-      >
-        프로젝트 입장
-      </Link>
     </div>
   );
 };
--- src/pages/ProjectRoom/ProjectRoom.tsx
+++ src/pages/ProjectRoom/ProjectRoom.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import { twMerge } from "tailwind-merge";
 import ProjectListBox from "../../components/ProjectRoom/ProjectListBox";
+import Button from "../../components/common/Button";
 
 const FILTER_PROJECT: (
   | "진행 완료 프로젝트"
@@ -19,44 +20,54 @@ const ProjectRoom = () => {
 
   return (
     <div
-      className="w-full bg-white p-[50px]"
+      className="w-full bg-white p-[50px] bg-gradient-to-t from-white/0 via-[#BFCDB7]/30 to-white/0"
       style={{ maxHeight: "calc(100vh - 50px)" }}
     >
       <div className="bg-white w-full h-full flex flex-col items-center gap-4 px-10">
-        {/* 프로젝트 필터링 */}
-        <ul
-          className="flex justify-center items-center gap-5 h-[70px] text-[18px]
-        font-bold text-black w-full "
-        >
-          {FILTER_PROJECT.map((project, idx) => {
-            return (
-              <li
-                key={idx}
-                onClick={() => setFilterProject(project)}
-                className={twMerge(
-                  `cursor-pointer border border-[#CAD2CB] text-[#CAD2CB] w-[200px] h-[30px] flex justify-center items-center rounded-[5px] ${
-                    filterProject === project && "text-black border-black"
-                  }`
-                )}
-              >
-                {project}
-              </li>
-            );
-          })}
-        </ul>
+        <div className="flex items-center gap-2 justify-around w-full">
+          {/* 프로젝트 필터링 */}
+          <ul
+            className="flex justify-start items-center gap-5 h-[70px] text-[18px]
+          font-bold text-black w-full"
+          >
+            {FILTER_PROJECT.map((project, idx) => {
+              return (
+                <li
+                  key={idx}
+                  onClick={() => setFilterProject(project)}
+                  className={twMerge(
+                    `cursor-pointer border border-[#CAD2CB] text-[#CAD2CB] w-[250px] h-[30px] flex justify-center items-center rounded-[5px] ${
+                      filterProject === project && "text-black border-black"
+                    }`
+                  )}
+                >
+                  {project}
+                </li>
+              );
+            })}
+          </ul>
+          <Button
+            text="프로젝트 선택"
+            size="md"
+            css="border-main-green01 text-main-green01 w-[120px] text-[14px] px-2"
+          />
+          <Button
+            text="+ 프로젝트 생성"
+            size="md"
+            css="border-none text-main-beige01 bg-main-green01 w-[130px] text-[14px] px-2"
+          />
+        </div>
 
         {/* 프로젝트 목록 섹션 */}
         <div
-          className="w-full flex flex-col gap-4 overflow-y-scroll flex-grow"
-          style={{ maxHeight: "calc(100vh - 270px)" }}
+          className="w-full max-h-[calc(100vh-220px)] flex flex-col gap-4 overflow-y-scroll scrollbar-none
+          flex-grow border border-main-green01 px-5 py-10 rounded-[10px]
+          bg-gradient-to-t from-[#BFCDB7]/10 via-[#BFCDB7]/0 to-[#BFCDB7]/10"
         >
           {arr.map((_, idx) => (
             <ProjectListBox key={idx} projectId={idx + 1} />
           ))}
         </div>
-
-        {/* 새 프로젝트 생성 */}
-        <div className="w-full h-[50px] bg-[#6E8370] rounded-[5px] cursor-pointer"></div>
       </div>
     </div>
   );
