77fd009ce9bcd3102f72560591fbf2ce38009a22
sunhyeong <parksunhyeong529@gmail.com>
2025-02-17 17:32:39 +0900

    design 프로젝트목록높이고정 #23

--- src/pages/MeetingRoom/MeetingRoom.tsx
+++ src/pages/MeetingRoom/MeetingRoom.tsx
@@ -4,10 +4,7 @@ import MeetingRoomProjectBox from "../../components/MeetingRoom/MeetingRoomProje
 
 const MeetingRoom = () => {
   return (
-    <div
-      className="flex w-full p-[50px] gap-[20px] bg-white bg-gradient-to-t from-white/0 via-[#BFCDB7]/30 to-white/0"
-      style={{ maxHeight: "calc(100vh - 50px)" }}
-    >
+    <div className="flex w-full h-[calc(100vh-50px)] p-[50px] gap-[20px] bg-white bg-gradient-to-t from-white/0 via-[#BFCDB7]/30 to-white/0">
       <div className="flex-[0.37] mb-[30px] flex flex-col gap-[30px] px-[30px] bg-white/60">
         <div className="ml-[10px]">
           <div className="inline-flex flex-col items-center">
@@ -20,6 +17,7 @@ const MeetingRoom = () => {
           </div>
         </div>
         <div className="w-full h-full flex flex-col gap-[20px] overflow-y-auto scrollbar-none">
+          <MeetingRoomProjectBox />
           <MeetingRoomProjectBox />
           <MeetingRoomProjectBox />
           <MeetingRoomProjectBox />
