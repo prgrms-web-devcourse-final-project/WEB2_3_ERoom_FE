440521bbf6041a758e7d182cc19212d0f61640a5
songwon-sung <swsung91@gmail.com>
2025-02-18 11:04:11 +0900

    fix: 프로젝트페이지 버튼 및 gap 사이즈 조정

--- src/pages/ProjectRoom/ProjectRoom.tsx
+++ src/pages/ProjectRoom/ProjectRoom.tsx
@@ -29,11 +29,11 @@ const ProjectRoom = () => {
       style={{ maxHeight: "calc(100vh - 50px)" }}
     >
       <div className="bg-white w-full h-full flex flex-col items-center gap-4 px-10">
-        <div className="flex items-center gap-2 justify-around w-full">
+        <div className="flex items-center gap-2 justify-between w-full">
           {/* 프로젝트 필터링 */}
           <ul
             className="flex justify-start items-center gap-5 h-[70px] text-[18px]
-          font-bold text-black w-full"
+          font-bold text-black w-full max-w-[660px]"
           >
             {FILTER_PROJECT.map((project, idx) => {
               return (
@@ -41,7 +41,7 @@ const ProjectRoom = () => {
                   key={idx}
                   onClick={() => setFilterProject(project)}
                   className={twMerge(
-                    `cursor-pointer border border-[#CAD2CB] text-[#CAD2CB] w-[250px] h-[30px] flex justify-center items-center rounded-[5px] ${
+                    `cursor-pointer border border-[#CAD2CB] text-[#CAD2CB] w-full h-[30px] flex justify-center items-center rounded-[5px] ${
                       filterProject === project && "text-black border-black"
                     }`
                   )}
@@ -53,7 +53,7 @@ const ProjectRoom = () => {
           </ul>
           {(filterProject === "진행 중인 프로젝트" ||
             filterProject === "진행 예정 프로젝트") && (
-            <>
+            <div className="flex w-[230px] gap-[10px]">
               <Button
                 text="프로젝트 선택"
                 size="md"
@@ -64,7 +64,7 @@ const ProjectRoom = () => {
                 size="md"
                 css="border-none text-main-beige01 bg-main-green01 w-[130px] text-[14px] px-2"
               />
-            </>
+            </div>
           )}
           {filterProject === "진행 완료 프로젝트" && (
             <>
