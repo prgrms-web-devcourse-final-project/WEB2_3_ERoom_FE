b88f644cf4a3e38a0fbf9d5c0ef3b506c8537bef
sunhyeong <parksunhyeong529@gmail.com>
2025-02-19 09:43:14 +0900

    design: 마이페이지알람모달수정 #46

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -6,13 +6,19 @@ import DefaultImg from "../assets/sample_default_profile.png";
 const MyPage = () => {
   const [companyInfo, setCompanyInfo] = useState<string | undefined>("");
   const [progileImg, setProfileImg] = useState("");
+  const [name, setName] = useState<string | undefined>("");
 
   const handleCompanyInfo = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCompanyInfo(e.target.value);
   };
+
   //프로필 이미지 수정 함수(추후 구현)
   const handleProfileImg = (e: React.ChangeEvent<HTMLInputElement>) => {
     setProfileImg(e.target.value);
+  };
+
+  const handleName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
   };
 
   return (
@@ -26,20 +32,27 @@ const MyPage = () => {
             </span>
           </div>
           <div className="flex gap-[30px] my-[50px]">
-            <div className="w-[200px] h-[206px] overflow-hidden cursor-pointer">
+            <button
+              onClick={() => handleProfileImg}
+              className="w-[200px] h-[206px] overflow-hidden cursor-pointer"
+            >
               {/* 프로필 기본 이미지 샘플로 넣어둠. 추후 기본이미지 나오면 수정 필요 */}
               <img
                 src={progileImg || DefaultImg}
                 alt="프로필 이미지"
                 className="w-full h-full object-cover"
               />
-            </div>
+            </button>
             <div className="flex flex-col gap-[20px]">
               <div className="flex flex-col gap-[10px]">
                 <span className="font-bold">이름</span>
-                <div className="pl-[10px]">
-                  <span className="text-black01">홍길동</span>
-                </div>
+                <input
+                  type="text"
+                  value={name ?? ""}
+                  onChange={handleName}
+                  placeholder="홍길동"
+                  className="w-[250px] h-[33px] pl-[10px] bg-transparent focus:outline-none border-b-[1px] border-b-gray01"
+                />
               </div>
               <div className="flex flex-col gap-[10px]">
                 <span className="font-bold">이메일</span>
