fa59576d6443338a81f9516eaf77dabbf22cbb8f
sunhyeong <parksunhyeong529@gmail.com>
2025-02-19 10:29:37 +0900

    design: 계정관리페이지수정 #50

--- src/components/Admin/Account.tsx
+++ src/components/Admin/Account.tsx
@@ -20,18 +20,15 @@ interface AccountListProps {
 
 const Account = () => {
   // 더미 데이터
-  const dummyUsers: AccountListProps[] = Array.from(
-    { length: 300 },
-    (_, i) => ({
-      id: i + 1,
-      email: `user${i + 1}@example.com`,
-      name: `사용자${i + 1}`,
-      registeredDate: `2025.02.${String(i + 1).padStart(2, "0")}`,
-      profileImage: "https://via.placeholder.com/50",
-      organization: `데브코스 프론트엔드 ${(i % 3) + 1}기`,
-      isSubscribed: i % 2 === 0,
-    })
-  );
+  const dummyUsers: AccountListProps[] = Array.from({ length: 4 }, (_, i) => ({
+    id: i + 1,
+    email: `user${i + 1}@example.com`,
+    name: `사용자${i + 1}`,
+    registeredDate: `2025.02.${String(i + 1).padStart(2, "0")}`,
+    profileImage: "https://via.placeholder.com/50",
+    organization: `데브코스 프론트엔드 ${(i % 3) + 1}기`,
+    isSubscribed: i % 2 === 0,
+  }));
 
   const [users, setUsers] = useState(dummyUsers);
 
@@ -74,7 +71,7 @@ const Account = () => {
 
   return (
     <div className="h-[calc(100vh-50px)] bg-gradient-to-t from-white/0 via-[#BFCDB7]/30 to-white/0">
-      <div className="mx-[30px] mb-[30px] px-[30px] pt-[30px] bg-white flex flex-col gap-[30px] bg-white/60">
+      <div className="min-h-[calc(100vh-80px)] mx-[30px] mb-[30px] px-[30px] pt-[30px] bg-white flex flex-col  bg-white/60">
         <div className="pl-[20px]">
           <span className="text-[22px] font-bold text-main-green">
             회원 계정 정보
@@ -101,7 +98,7 @@ const Account = () => {
             <img src={DeleteIcon} />
           </button>
         </div>
-        <div className="flex flex-col gap-[10px]">
+        <div className="flex flex-col gap-[10px] flex-grow mb-[30px]">
           {/* 제목 부분 */}
           <div className="grid grid-cols-[5%_5%_30%_25%_25%_10%] h-[36px] w-full text-main-green text-[14px] border-b border-b-header-green">
             <div className="flex justify-center items-center">
@@ -135,7 +132,7 @@ const Account = () => {
             />
           ))}
         </div>
-        <div className="flex justify-center items-center mt-[20px]">
+        <div className="flex justify-center items-center mt-auto mb-[30px]">
           <Pagination totalPages={totalPages} onPageChange={setCurrentPage} />
         </div>
       </div>
