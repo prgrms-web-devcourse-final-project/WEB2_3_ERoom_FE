30a9282bbaa7ec999cf91fef864114d0fce79445
songwon-sung <swsung91@gmail.com>
2025-02-20 09:48:14 +0900

    FIX: 분야 선택 시에만 세부항목 보이도록 분기처리

--- src/components/EditProjectModal/SelectCategory.tsx
+++ src/components/EditProjectModal/SelectCategory.tsx
@@ -129,97 +129,100 @@ const SelectCategory = ({
       <div className="flex flex-col gap-[5px]">
         {selectedData.cate && <p className="w-full font-bold">세부항목</p>}
       </div>
-      {categoryData
-        .filter((data) => data.name === selectedData.cate)[0]
-        .subcategories?.map((subcate, index) => {
-          const subcategoryKey = subcate.subname;
-          const items = subcate.data;
+      {selectedData.cate &&
+        categoryData
+          .filter((data) => data.name === selectedData.cate)[0]
+          .subcategories?.map((subcate, index) => {
+            const subcategoryKey = subcate.subname;
+            const items = subcate.data;
 
-          if (!items) return null;
+            if (!items) return null;
 
-          return (
-            <div key={index}>
-              {/* 세부항목 선택창 */}
-              <div
-                key={index}
-                className="flex flex-col gap-[10px] w-full border-[1px] border-gray01 rounded-[2px] px-[10px] py-[5px] text-center 
+            return (
+              <div key={index}>
+                {/* 세부항목 선택창 */}
+                <div
+                  key={index}
+                  className="flex flex-col gap-[10px] w-full border-[1px] border-gray01 rounded-[2px] px-[10px] py-[5px] text-center 
                 text-gray01 text-[14px] font-bold"
-              >
-                <div className="flex justify-between">
-                  {/* 세부항목명 */}
-                  <div>{subcategoryKey}</div>
+                >
+                  <div className="flex justify-between">
+                    {/* 세부항목명 */}
+                    <div>{subcategoryKey}</div>
 
-                  {/* 토글 버튼 */}
-                  <img
-                    src={
-                      isClicked.subcategories[index] ? triangleUp : triangleDown
-                    }
-                    className="cursor-pointer"
-                    onClick={() => toggleSubcategory(index)}
-                  />
+                    {/* 토글 버튼 */}
+                    <img
+                      src={
+                        isClicked.subcategories[index]
+                          ? triangleUp
+                          : triangleDown
+                      }
+                      className="cursor-pointer"
+                      onClick={() => toggleSubcategory(index)}
+                    />
+                  </div>
+
+                  {/* 클릭 시 드롭다운 목록 */}
+                  {isClicked.subcategories[index] && (
+                    <div className="flex flex-col overflow-y-auto h-[150px] scrollbar">
+                      <div className="flex flex-col gap-[10px]">
+                        {/* 구분선 */}
+                        <hr />
+
+                        {/* 상세항목 목록 */}
+                        {items.map((item, idx) => (
+                          <div
+                            key={idx}
+                            className="hover:text-logo-green hover:bg-main-green02"
+                            onClick={() =>
+                              handleSelectedCategories(item.text, index)
+                            }
+                          >
+                            {item.text}
+                          </div>
+                        ))}
+                      </div>
+                    </div>
+                  )}
                 </div>
 
-                {/* 클릭 시 드롭다운 목록 */}
-                {isClicked.subcategories[index] && (
-                  <div className="flex flex-col overflow-y-auto h-[150px] scrollbar">
-                    <div className="flex flex-col gap-[10px]">
-                      {/* 구분선 */}
-                      <hr />
-
-                      {/* 상세항목 목록 */}
-                      {items.map((item, idx) => (
-                        <div
-                          key={idx}
-                          className="hover:text-logo-green hover:bg-main-green02"
-                          onClick={() =>
-                            handleSelectedCategories(item.text, index)
-                          }
-                        >
-                          {item.text}
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                )}
-              </div>
-
-              {/* 첫 번째 선택항목 표시 */}
-              {index === 0 &&
-                selectedData.subcate1.map((item) => (
-                  <div
-                    className="flex justify-between text-logo-green text-[14px]
+                {/* 첫 번째 선택항목 표시 */}
+                {index === 0 &&
+                  selectedData.subcate1.map((item) => (
+                    <div
+                      className="flex justify-between text-logo-green text-[14px]
                       py-[5px] px-[10px] font-bold"
-                  >
-                    {/* 선택항목명 */}
-                    <div key={item}>{item}</div>
+                    >
+                      {/* 선택항목명 */}
+                      <div key={item}>{item}</div>
 
-                    {/* 취소버튼 */}
-                    <img
-                      src={cancelButton}
-                      onClick={() => handleSelectedCategories(item, index)}
-                      className="cursor-pointer"
-                    />
-                  </div>
-                ))}
+                      {/* 취소버튼 */}
+                      <img
+                        src={cancelButton}
+                        onClick={() => handleSelectedCategories(item, index)}
+                        className="cursor-pointer"
+                      />
+                    </div>
+                  ))}
 
-              {/* 두 번째 선택항목 표시 */}
-              {index === 1 &&
-                selectedData.subcate2.map((item) => (
-                  <div
-                    className="flex justify-between text-logo-green text-[14px]
+                {/* 두 번째 선택항목 표시 */}
+                {index === 1 &&
+                  selectedData.subcate2.map((item) => (
+                    <div
+                      className="flex justify-between text-logo-green text-[14px]
                     py-[5px] px-[10px] font-bold"
-                  >
-                    <div key={item}>{item}</div>
-                    <img
-                      src={cancelButton}
-                      onClick={() => handleSelectedCategories(item, index)}
-                      className="cursor-pointer"
-                    />
-                  </div>
-                ))}
-            </div>
-          );
-        })}
+                    >
+                      <div key={item}>{item}</div>
+                      <img
+                        src={cancelButton}
+                        onClick={() => handleSelectedCategories(item, index)}
+                        className="cursor-pointer"
+                      />
+                    </div>
+                  ))}
+              </div>
+            );
+          })}
     </div>
   );
 };
