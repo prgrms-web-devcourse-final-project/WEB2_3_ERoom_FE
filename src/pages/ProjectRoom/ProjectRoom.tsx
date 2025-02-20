2d311b4c93ee871b2b92d82870c2eee79491bf11
songwon-sung <swsung91@gmail.com>
2025-02-20 16:26:40 +0900

    FEAT/FIX: 프로젝트생성편집모달에 기간설정 추가

--- src/pages/ProjectRoom/ProjectRoom.tsx
+++ src/pages/ProjectRoom/ProjectRoom.tsx
@@ -130,7 +130,6 @@ const ProjectRoom = () => {
           onClick={() => setIsEditProjectModal(false)}
         >
           <EditProjectModal
-            selectedProjectData={selectedProjectData}
             setIsEditProjectModal={setIsEditProjectModal}
             title="프로젝트 생성"
           />
