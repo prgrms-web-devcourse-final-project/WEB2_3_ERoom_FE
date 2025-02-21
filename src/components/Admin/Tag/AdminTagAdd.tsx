76a98647c65f8d29945d7e12948edaed17c0c35b
sunhyeong <parksunhyeong529@gmail.com>
2025-02-21 16:20:39 +0900

    design: 태그관리페이지 #69

--- src/components/Admin/Tag/AdminTagAdd.tsx
+++ src/components/Admin/Tag/AdminTagAdd.tsx
@@ -1,32 +1,61 @@
 import EditButton from "../../../assets/icons/edit.svg";
 import DeleteButton from "../../../assets/icons/delete.svg";
+import SaveButton from "../../../assets/icons/save.svg";
+import { useState } from "react";
 
-interface AdminTagListProps {
+interface AdminTagAddProps {
   index: number;
-  name: string | null;
-  handleChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  name: string;
+  id: string;
+  onChange: (id: string, newName: string) => void;
 }
 
-const AdminTagAdd = ({ index, name, handleChange }: AdminTagListProps) => {
+const AdminTagAdd = ({ index, name, id, onChange }: AdminTagAddProps) => {
+  const [isEditable, setIsEditable] = useState(true);
+  const [value, setValue] = useState(name);
+
+  const handleSave = () => {
+    onChange(id, value);
+    setIsEditable(false);
+  };
+
   return (
     <div className="grid grid-cols-[9.4%_1fr_12.5%_12.5%] w-full h-[33px] text-main-green ">
       <div className="flex justify-center">
         <span>{index + 1}</span>
       </div>
       <div className="flex w-full justify-center ">
-        <textarea
-          value={name || ""}
-          onChange={(e) => handleChange(e)}
-          placeholder="분야를 작성해주세요"
-          className="border-b border-b-header-green resize-none text-[14px] h-[25px] w-full text-center"
-        />
+        {isEditable ? (
+          <input
+            type="text"
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            maxLength={20}
+            className="resize-none text-[14px] h-[25px] w-full text-center focus:outline-none spellcheck-false overflow-hidden whitespace-nowrap text-ellipsis border-b border-b-header-green"
+          />
+        ) : (
+          <span className="text-[14px] text-center">{name}</span>
+        )}
       </div>
-      <div className="flex justify-center">
-        <img src={EditButton} alt="수정 버튼" className="w-[25px] h-[25px]" />
-      </div>
-      <div className="flex justify-center">
+      {!isEditable ? (
+        <button
+          className="flex justify-center cursor-pointer"
+          onClick={() => setIsEditable(true)}
+        >
+          <img src={EditButton} alt="수정 버튼" className="w-[25px] h-[25px]" />
+        </button>
+      ) : (
+        <button
+          className="flex justify-center cursor-pointer"
+          onClick={handleSave}
+        >
+          <img src={SaveButton} alt="저장 버튼" className="w-[25px] h-[25px]" />
+        </button>
+      )}
+
+      <button className="flex justify-center cursor-pointer">
         <img src={DeleteButton} alt="삭제 버튼" className="w-[25px] h-[25px]" />
-      </div>
+      </button>
     </div>
   );
 };
