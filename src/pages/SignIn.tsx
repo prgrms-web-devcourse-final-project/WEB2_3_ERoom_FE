ab3fee853ff3abd73f8b135d8bc9fa5019ae8065
songwon-sung <swsung91@gmail.com>
2025-02-21 11:18:13 +0900

    FIX: 로그인 디자인 변경 - 회원가입 삭제

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -5,33 +5,30 @@ import GoogleLogo from "../assets/google_logo.svg";
 
 const SignIn = () => {
   return (
-    <div className=" w-[554px] h-[414px] bg-[#ffffff94] px-[100px] py-[50px] rounded-[10px]">
-      <div className="">
-        <div className="text-center mb-[50px]">
-          <span className="text-logo-green-light font-bold text-[20px]">
-            LOGIN
-          </span>
-        </div>
-        <div className="relative flex flex-col gap-[20px] pb-[40px] border-b-[1px]">
-          <Button
-            text="카카오 로그인"
-            size="lg"
-            css="bg-kakao border-gray01 text-black gap-[10px]"
-            logo={KakaoLogo}
-          />
-          <Button
-            text="구글 로그인"
-            size="lg"
-            css="bg-white border-gray01 text-black gap-[10px]"
-            logo={GoogleLogo}
-          />
-        </div>
-        <div className="relative flex flex-col pt-[40px] gap-[5px]">
-          <span className="text-sm text-center font-semibold">
-            아직 회원이 아니신가요?
-          </span>
-          <Button text="회원가입" to="/signup" size="lg" />
-        </div>
+    <div
+      className="w-[554px] h-full bg-[#ffffff94] px-[100px] py-[50px] 
+    flex flex-col gap-[50px] rounded-[10px]"
+    >
+      {/* LOGIN */}
+      <div className="text-center">
+        <p className="text-logo-green-light font-bold text-[20px]">LOGIN</p>
+      </div>
+
+      {/* 버튼 모음 */}
+      {/* 버튼 글릭 시 회원가입 여부를 확인하는 로직 추가 필요 */}
+      <div className="flex flex-col gap-[20px]">
+        <Button
+          text="카카오 로그인"
+          size="lg"
+          css="bg-kakao border-gray01 text-black gap-[10px]"
+          logo={KakaoLogo}
+        />
+        <Button
+          text="구글 로그인"
+          size="lg"
+          css="bg-white border-gray01 text-black gap-[10px]"
+          logo={GoogleLogo}
+        />
       </div>
     </div>
   );
