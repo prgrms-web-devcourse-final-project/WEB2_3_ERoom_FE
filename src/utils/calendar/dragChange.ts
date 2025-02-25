ac98f6add28138b32aac5d3f1908736ba7edbe8d
kkyuheak <gyuheak@gmail.com>
2025-02-25 16:26:51 +0900

    feat: 캘린더 프로젝트 수정기능 api연결 #125

--- src/utils/calendar/dragChange.ts
+++ src/utils/calendar/dragChange.ts
@@ -5,11 +5,18 @@ import { EventDropArg } from "@fullcalendar/core/index.js";
 // 캘린더 일정 드래그시 호출함수
 export const dragChange = async (info: EventDropArg) => {
   // 변경된 일정 날짜 포맷팅
-  const startDate = dayjs(info.event.start).format("YYYY-MM-DD");
-  const endDate = dayjs(info.event.end).format("YYYY-MM-DD");
+  const startDate = dayjs(info.event.start).format("YYYY-MM-DDTHH:mm:ss");
+  const endDate = dayjs(info.event.end).format("YYYY-MM-DDTHH:mm:ss");
+
+  const projectData = info.event.extendedProps;
 
   try {
-    const response = await api.patch(`/projects/${info.event.id}`, {
+    const response = await api.patch(`/api/projects/${info.event.id}/update`, {
+      name: info.event.title,
+      category: projectData.catagory,
+      subCategories1: projectData.subCategories1,
+      subCategories2: projectData.subCategories2,
+      status: projectData.status,
       startDate,
       endDate,
     });
