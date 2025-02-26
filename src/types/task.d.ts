5185ef869f2442eeb477a044f7f8b36a8296eb75
songwon-sung <swsung91@gmail.com>
2025-02-26 16:59:52 +0900

    fix: 임시 적용을 위한 업무 타입 수정

--- src/types/task.d.ts
+++ src/types/task.d.ts
@@ -15,6 +15,9 @@ interface selectedDateType {
 
 interface CreateTaskProps {
   onClose: React.Dispatch<React.SetStateAction<boolean>>;
+  setData: React.Dispatch<React.SetStateAction<data>>;
+  projectId: number;
+  setIsClicked: React.Dispatch<React.SetStateAction<isClicked>>;
 }
 
 interface TaskListProps {
@@ -44,6 +47,17 @@ interface Task {
   assignedMemberName: string;
   assignedMemberProfile: string; // URL 형식으로 처리
   participants: string[]; // 참가자는 문자열 배열로 처리
+}
+
+interface CreateTask {
+  projectId: number;
+  title: string;
+  startDate: string; // ISO 형식의 날짜 문자열로 취급
+  endDate: string; // ISO 형식의 날짜 문자열로 취급
+  status: "IN_PROGRESS" | "COMPLETED" | "BEFORE_START" | "HOLD"; // 상태값이 정해져 있을 경우 문자열 리터럴 타입을 사용
+  assignedMemberId: number;
+  participantIds: number[]; // 참가자는 문자열 배열로 처리
+  colors?: { background: string; text: string };
 }
 
 interface ManageTasksType {
