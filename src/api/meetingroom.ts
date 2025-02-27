844246c935bbcd1bac6cab99f40c2ae9e6f739b5
sunhyeong <parksunhyeong529@gmail.com>
2025-02-27 16:24:38 +0900

    feat: 회의록생성 #137

--- src/api/meetingroom.ts
+++ src/api/meetingroom.ts
@@ -13,3 +13,21 @@ export const getMeetingroom = async (
     throw error;
   }
 };
+
+//AI 회의록 가져오는 API
+export const getAINote = async (
+  chatRoomId: number,
+  startTime: string,
+  endTime: string
+): Promise<AINoteType> => {
+  try {
+    const response = await api.get(
+      `/api/report/${chatRoomId}?startTime=${startTime}&endTime=${endTime}`
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error("AI회의록 불러오기 에러", error);
+    throw error;
+  }
+};
