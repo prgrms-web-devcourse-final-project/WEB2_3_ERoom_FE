a5d0a7230f2f73f0df92def89ff1229daccb75c7
songwon-sung <swsung91@gmail.com>
2025-03-04 23:24:06 +0900

    feat: 관리자 업무 데이터 연결 및 수정

--- src/components/Admin/Task/AdminTaskList.tsx
+++ src/components/Admin/Task/AdminTaskList.tsx
@@ -8,16 +8,15 @@ import { PROGRESS_STATUS } from "../../../constants/status";
 import ProgressStatusBox from "../ProgressStatusBox";
 import { progressType } from "../../../utils/progressType";
 
-interface TasksListType {
-  id: number;
+interface TaskList {
+  taskId: number;
   taskName: string;
-  manager: string;
   projectName: string;
+  assignedMember: string;
+  assignedEmail: string;
   taskStatus: string;
-  createdAt: string;
   startDate: string;
   endDate: string;
-  isActive: boolean;
 }
 
 const AdminTaskList = ({
@@ -25,35 +24,54 @@ const AdminTaskList = ({
   index,
   onUpdateTask,
 }: {
-  task: TasksListType;
+  task: TaskList;
   index: number;
-  onUpdateTask: (id: number, updatedTask: Partial<TasksListType>) => void;
+  onUpdateTask: (id: number, updatedTask: TaskList) => void;
 }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [editedTask, setEditedTask] = useState({ ...task });
+  // 체크 상태
+  const [isChecked, setIsChecked] = useState(false);
+  // 진행상태 체크
+  const [status, setStatus] = useState<string>(
+    PROGRESS_STATUS[task.taskStatus]!
+  );
 
+  // 저장 함수
   const handleSaveClick = () => {
     setIsEditing(false);
-    onUpdateTask(task.id, editedTask);
+    onUpdateTask(task.taskId, editedTask);
+  };
+  // console.log(editedTask);
+
+  // 기간 데이터 형식 변경 함수
+  const formatDate = (isoString: string) => {
+    const date = new Date(isoString);
+    return date
+      .toLocaleDateString("ko-KR", {
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+      })
+      .replace(/\. /g, "."); // 공백 제거
   };
 
+  // 기간 데이터 형식 변경
+  const formattedStartDate = formatDate(task.startDate);
+  const formattedEndDate = formatDate(task.endDate);
+
+  // 수정/저장버튼 클릭 함수
   const handleEditClick = () => setIsEditing(true);
 
+  // 내용수정 함수
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
     setEditedTask({ ...editedTask, [name]: value });
   };
 
-  const [isChecked, setIsChecked] = useState(false);
-
   const toggleCheckBox = () => {
     setIsChecked((prev) => !prev);
   };
-
-  // 진행상태 체크
-  const [status, setStatus] = useState<string>(
-    PROGRESS_STATUS[task.taskStatus]!
-  );
 
   useEffect(() => {
     console.log(status);
@@ -66,14 +84,15 @@ const AdminTaskList = ({
   return (
     <div
       className={twMerge(
-        "flex flex-col",
+        "flex flex-row",
         isChecked || isEditing ? "bg-main-green03" : "bg-transparent"
       )}
     >
       <div
-        className="grid grid-cols-[5%_5%_15%_20%_10%_10%_10%_20%_5%] h-[37px] w-full 
+        className="grid grid-cols-[5%_5%_10%_10%_10%_25%_10%_20%_5%] h-full w-full 
         text-main-green text-[14px] py-[5px]"
       >
+        {/* 체크박스 */}
         <div className="flex justify-center items-center">
           <button
             onClick={(e) => {
@@ -85,9 +104,12 @@ const AdminTaskList = ({
             <img src={isChecked ? CheckBox : UnCheckBox} alt="체크박스" />
           </button>
         </div>
+
+        {/* 넘버 */}
         <div className="flex justify-center items-center">
           <span>{index + 1}</span>
         </div>
+
         {/* 업무 명 */}
         <div className="flex justify-center items-center">
           {isEditing ? (
@@ -100,28 +122,35 @@ const AdminTaskList = ({
               style={{ width: `${editedTask.taskName.length + 2}ch` }}
             />
           ) : (
-            <span>{task.taskName}</span>
+            <p
+              className="whitespace-nowrap overflow-hidden text-ellipsis 
+              hover:whitespace-pre-wrap hover:overflow-visible"
+            >
+              {task.taskName}
+            </p>
           )}
         </div>
+
         {/* 프로젝트 명 */}
         <div className="flex items-center justify-center">
-          <p>{task.projectName}</p>
+          <p
+            className="whitespace-nowrap overflow-hidden text-ellipsis 
+              hover:whitespace-pre-wrap hover:overflow-visible"
+          >
+            {task.projectName}
+          </p>
         </div>
+
         {/* 담당자 */}
         <div className="flex items-center justify-center">
-          {isEditing ? (
-            <input
-              type="text"
-              name="manager"
-              value={editedTask.manager}
-              onChange={handleInputChange}
-              className="h-full w-auto text-center focus:outline-none border-b border-b-header-green"
-              style={{ width: `${editedTask.manager.length + 2}ch` }}
-            />
-          ) : (
-            <span>{task.manager}</span>
-          )}
+          <p>{task.assignedMember}</p>
         </div>
+
+        <div className="flex justify-center items-center relative">
+          <span>{task.assignedEmail}</span>
+        </div>
+
+        {/* 진행상태 */}
         <div className="flex justify-center items-center relative">
           {/* 드롭다운박스 */}
           {isEditing ? (
@@ -134,25 +163,27 @@ const AdminTaskList = ({
             <span>{status}</span>
           )}
         </div>
+
+        {/* 기간 */}
+        <div className="flex justify-center items-center">
+          <p>{formattedStartDate}</p>
+          <p>~ {formattedEndDate}</p>
+        </div>
+
+        {/* 수정/저장 버튼 */}
         <div className="flex justify-center items-center">
           {isEditing ? (
-            <span>{task.createdAt}</span>
-          ) : (
-            <span>{task.createdAt}</span>
-          )}
-        </div>
-        <div className="flex justify-center items-center">
-          <p>
-            {task.startDate} - {task.endDate}
-          </p>
-        </div>
-        <div className="flex justify-center items-center">
-          {isEditing ? (
-            <button onClick={handleSaveClick} className="cursor-pointer">
+            <button
+              onClick={handleSaveClick}
+              className="cursor-pointer w-[37px] h-[27px]"
+            >
               <img src={SaveIcon} alt="저장" />
             </button>
           ) : (
-            <button onClick={handleEditClick} className="cursor-pointer">
+            <button
+              onClick={handleEditClick}
+              className="cursor-pointer w-[37px] h-[27px]"
+            >
               <img src={EditIcon} alt="수정" />
             </button>
           )}
