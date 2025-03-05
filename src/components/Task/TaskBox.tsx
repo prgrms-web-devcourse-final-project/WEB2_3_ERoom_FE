0260e4d05ed9ee1b5eb671efb82250a900f7d899
songwon-sung <swsung91@gmail.com>
2025-03-05 15:08:59 +0900

    fix: 담당업무만 수정가능 및 hover시 디자인 추가

--- src/components/Task/TaskBox.tsx
+++ src/components/Task/TaskBox.tsx
@@ -3,8 +3,12 @@ import { PROGRESS_STATUS } from "../../constants/status";
 import Button from "../common/Button";
 import ParticipantIcon from "../common/ParticipantIcon";
 import { getTaskById } from "../../api/task";
+import { useAuthStore } from "../../store/authStore";
 
 const TaskBox = ({ isAll = true, onClick, task, onUpdate }: TaskBoxProps) => {
+  const { member } = useAuthStore();
+
+  // 업무 수정
   const { data: updatedData } = useQuery<GetUpdateTask>({
     queryKey: ["UpdatedData", task.taskId],
     queryFn: async () => {
@@ -45,8 +49,12 @@ const TaskBox = ({ isAll = true, onClick, task, onUpdate }: TaskBoxProps) => {
   if (isAll && updatedData) {
     return (
       <div
-        className="w-[320px] h-[120px] bg-white border border-main-green02
-        px-3 py-2 flex flex-col justify-center gap-2 cursor-pointer"
+        className={`w-[320px] h-[120px] bg-white border border-main-green02
+        px-3 py-2 flex flex-col justify-center gap-2 ${
+          task.assignedMemberName === member?.username &&
+          "cursor-pointer transition-all duration-500 transform hover:scale-105 hover:border-[3px] hover:border-main-green01 hover:rounded-[10px]"
+        } 
+        `}
         onClick={onClick}
       >
         <div className="flex justify-between items-center">
@@ -62,27 +70,28 @@ const TaskBox = ({ isAll = true, onClick, task, onUpdate }: TaskBoxProps) => {
             {updatedData.startDate.split("T")[0]} ~{" "}
             {updatedData.endDate.split("T")[0]}
           </p>
-          {task.status !== "IN_PROGRESS" ? (
-            <Button
-              text={"시작"}
-              size="md"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleStateStart();
-              }}
-              css="border-main-green01 h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-green01 bg-main-green02"
-            />
-          ) : (
-            <Button
-              text={"완료"}
-              size="md"
-              onClick={(e) => {
-                e.stopPropagation();
-                handleCompleteStart();
-              }}
-              css="border-none h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-beige01 bg-main-green01"
-            />
-          )}
+          {task.assignedMemberName === member?.username &&
+            (task.status !== "IN_PROGRESS" ? (
+              <Button
+                text={"시작"}
+                size="md"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleStateStart();
+                }}
+                css="border-main-green01 h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-green01 bg-main-green02"
+              />
+            ) : (
+              <Button
+                text={"완료"}
+                size="md"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleCompleteStart();
+                }}
+                css="border-none h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-beige01 bg-main-green01"
+              />
+            ))}
         </div>
 
         {/* 담당자 */}
@@ -105,11 +114,19 @@ const TaskBox = ({ isAll = true, onClick, task, onUpdate }: TaskBoxProps) => {
     /* 로그인 유저의 업무인지 확인하여 편집가능하게 해야 함 */
     return (
       <div
-        className="w-[300px] h-[80px] bg-white border border-main-green02 
-        px-3 py-2 flex flex-col justify-center gap-2"
+        className={`w-[300px] h-[80px] bg-white border border-main-green02
+        px-3 py-2 flex flex-col justify-center gap-2 ${
+          task.assignedMemberName === member?.username &&
+          "cursor-pointer transition-all duration-500 transform hover:scale-105 hover:border-[3px] hover:border-main-green01 hover:rounded-[10px]"
+        } 
+        `}
+        onClick={onClick}
       >
         <div className="flex justify-between items-center">
+          {/* 업무명 */}
           <p className="font-bold">{task.title}</p>
+
+          {/* 진행상태 */}
           <p className="text-[12px] font-medium">
             {PROGRESS_STATUS[task.status]}
           </p>
@@ -120,19 +137,20 @@ const TaskBox = ({ isAll = true, onClick, task, onUpdate }: TaskBoxProps) => {
           <p className="text-[12px]">
             {task.startDate.split("T")[0]} ~ {task.endDate.split("T")[0]}
           </p>
-          {task.status !== "IN_PROGRESS" ? (
-            <Button
-              text={"시작"}
-              size="md"
-              css="border-main-green01 h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-green01 bg-main-green02"
-            />
-          ) : (
-            <Button
-              text={"완료"}
-              size="md"
-              css="border-none h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-beige01 bg-main-green01"
-            />
-          )}
+          {task.assignedMemberName === member?.username &&
+            (task.status !== "IN_PROGRESS" ? (
+              <Button
+                text={"시작"}
+                size="md"
+                css="border-main-green01 h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-green01 bg-main-green02"
+              />
+            ) : (
+              <Button
+                text={"완료"}
+                size="md"
+                css="border-none h-[22px] w-fit px-[10px] py-[2px] font-normal text-[14px] rounded-[4px] text-main-beige01 bg-main-green01"
+              />
+            ))}
         </div>
       </div>
     );
