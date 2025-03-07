87a64f44a68c74854e97bbc7eaec5fae374bb8c3
songwon-sung <swsung91@gmail.com>
2025-03-08 01:06:08 +0900

    fix: 불필요한 부분 삭제

--- src/components/Task/TaskList.tsx
+++ src/components/Task/TaskList.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import TaskBox from "./TaskBox";
 import UpdateTaskModal from "../modals/UpdateTaskModal";
 import { useMutation } from "@tanstack/react-query";
