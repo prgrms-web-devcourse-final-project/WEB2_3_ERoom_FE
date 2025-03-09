a6099f4785607cb7a8eff6e9755da710fe8ccb52
songwon-sung <swsung91@gmail.com>
2025-03-09 21:23:17 +0900

    feat: 워드클라우드 추가

--- src/components/EditProjectModal/WordCloud.tsx
+++ src/components/EditProjectModal/WordCloud.tsx
@@ -1,77 +1,38 @@
 import React, { useRef, useEffect } from "react";
-import * as d3 from "d3";
-import cloud from "d3-cloud";
-import { scaleSequential } from "d3-scale";
+import WordCloudForm from "wordcloud";
 
 const WordCloud: React.FC<WordCloudProps> = ({ words }) => {
-  const svgRef = useRef<SVGSVGElement>(null);
-  const width = 300;
-  const height = 250;
-
-  // console.log(words);
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
 
   useEffect(() => {
-    if (!svgRef.current) return;
+    if (canvasRef.current) {
+      const wordArray: [string, number][] = words.map((word) => [
+        word.text,
+        word.value,
+      ]);
 
-    const svg = d3.select(svgRef.current);
-    svg.selectAll("*").remove(); // 기존 SVG 내용 제거
+      const gridSize = Math.round(
+        (16 * Math.max(...words.map((word) => word.value))) / 1024
+      );
 
-    const layout = cloud<Word>()
-      .size([width - 50, height - 50])
-      .words(
-        words.map((word) => ({
-          text: word.text,
-          size: word.value,
-        }))
-      )
-      .padding((d) => Math.log(d.size) * 2)
-      .rotate(() => (Math.random() > 0.5 ? 0 : 90)) // 가독성 고려
-      .font("Impact")
-      .fontSize((d) => Math.min(30, Math.max(10, d.size)))
-      .spiral("archimedean")
-      .random(Math.random) // random 문제 해결
-      .on("end", draw);
+      const weightFactor = (size: number): number => {
+        if (!canvasRef.current) return 0; // canvas가 없을 경우 0을 반환
+        return (Math.pow(size, 2.8) * canvasRef.current.width) / 1024;
+      };
 
-    layout.start();
-
-    function draw(words: Word[]) {
-      const g = svg
-        .append("g")
-        .attr(
-          "transform",
-          `translate(${layout.size()[0] / 2},${layout.size()[1] / 2})`
-        );
-
-      g.selectAll("text")
-        .data(words)
-        .enter()
-        .append("text")
-        .style("font-size", (d) => `${d.size}px`)
-        .style("font-family", "Impact")
-        .style("fill", (d) => {
-          const colorScale = scaleSequential(d3.interpolateGreens).domain([
-            d3.min(words, (d) => d.size) || 10,
-            d3.max(words, (d) => d.size) || 140,
-          ]);
-          return colorScale(d.size); // 글자 크기에 따라 색상이 변하도록 설정
-        })
-        .attr("text-center", "middle")
-        .attr(
-          "transform",
-          (d) => `translate(${d.x},${d.y}) rotate(${d.rotate})`
-        )
-        .text((d) => d.text);
+      WordCloudForm(canvasRef.current, {
+        list: wordArray,
+        gridSize: gridSize,
+        weightFactor: weightFactor, // 크기 조정
+        fontFamily: "Impact",
+        color: "random-dark", // 글자 색상
+        backgroundColor: "#ffffff", // 배경 색상
+        rotateRatio: 0.5, // 회전 비율
+      });
     }
   }, [words]);
 
-  return (
-    <svg
-      className="border-[1px] border-gray01"
-      ref={svgRef}
-      width="300"
-      height="250"
-    ></svg>
-  );
+  return <canvas ref={canvasRef} width={300} height={250}></canvas>;
 };
 
 export default WordCloud;
